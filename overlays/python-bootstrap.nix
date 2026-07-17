3ebf054755d3fe3d9206ea2e29d4b6f514ca0f35
Pavel Sieder <pavel@sieder.cz>
2026-07-17 10:47:27 +0200

    Optimized Rpi5 builds for Cortex-A76

--- overlays/python-bootstrap.nix
+++ overlays/python-bootstrap.nix
@@ -53,7 +53,9 @@ final: prev: {
                 ${python.interpreter} -m installer \
                   --destdir "$out" --prefix "" dist/*.whl
 
-              wrapProgram "$out/bin/pyproject-build" \
+              rm -f "$out/bin/pyproject-build"
+              makeWrapper ${python.interpreter} "$out/bin/pyproject-build" \
+                --add-flags "-m build" \
                 --prefix PYTHONPATH : "$out/${sitePkgs}" \
                 --prefix PYTHONPATH : "${bootstrap-pyproject-hooks}/${sitePkgs}" \
                 --prefix PYTHONPATH : "${bootstrap-packaging}/${sitePkgs}" \
